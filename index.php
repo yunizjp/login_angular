78b096b7c655d1969d08bac24355f4e2cb3d2b5d
yunizjp <yunizjp@gmail.com>
2023-02-08 13:28:58 -0500

    creacion de proyecto

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login de usuario</title>
+</head>
+<body>
+    <div>
+        <h1>Ingreso al sistema</h1>
+
+        <div>
+            <form action="">
+                <label class="text" placeholder="Ingrese su nombre">Nombre: </label>
+                <input type="text" class="text">
+
+                <label class="text" placeholder="Ingrese su contraseña">Contraseña: </label>
+                <input type="password" class="text">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
